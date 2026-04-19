2e4144910248818c838b9b4176e0d73e4f064267
kennypele <khaliffa2001@yahoo.com>
2026-04-18 21:12:16 -0500

    here comes

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html", app_name="Flask Elastic Beanstalk Demo version 6.0")
+    return render_template("index.html", app_name="Flask Elastic Beanstalk Demo version 7.0")
 
 
 @app.route("/health")
